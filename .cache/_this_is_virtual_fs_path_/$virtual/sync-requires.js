3304e364351381040cb1c650e5ff6da2ceb09b13
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-08-14 17:20:05 +0300

    move folder

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\user\\OneDrive\\Desktop\\Tom Mboya\\.cache\\dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\user\\OneDrive\\Desktop\\Tom Mboya\\src\\pages\\index.js"))
+}
+
---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,19 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-styled-components/gatsby-browser.js'),
+      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"topLevelImportPaths":[],"pure":false,"disableVendorPrefixes":false},
+    },{
+      plugin: require('../node_modules/gatsby-background-image/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"typekit":{}},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#EAE4E4","theme_color":"#121111","display":"minimal-ui","icon":"src/images/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"c2339c981989b9f3682a7b38d7588a3b"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
